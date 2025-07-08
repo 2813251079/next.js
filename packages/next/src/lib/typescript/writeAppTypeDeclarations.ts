c46f2aec487f4ee045cbfd15726714b1661ee145
Ben Gubler <nebrelbug@gmail.com>
2025-07-08 02:57:17 -0700

    feat: type guard file

--- packages/next/src/lib/typescript/writeAppTypeDeclarations.ts
+++ packages/next/src/lib/typescript/writeAppTypeDeclarations.ts
@@ -59,6 +59,7 @@ export async function writeAppTypeDeclarations({
 
   if (hasTypedRoutes) {
     directives.push('/// <reference path="./.next/types/routes.ts" />')
+    directives.push('/// <reference path="./.next/types/validator.ts" />')
   }
 
   // Push the notice in.
